7f116df50f70dc0fc773a7d39c8c4c2ed88975c6
gfaurie <gabrielle.faurie@savoye.com>
2021-05-05 20:26:04 +0200

    Init project with modules

---
+++ main-project/time/src/main/java/com/webservices/TimeApiRestApplication.java
@@ -0,0 +1,11 @@
+package com.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeApiRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TimeApiRestApplication.class, args);
+	}
+}
---
+++ main-project/time/src/main/java/com/webservices/datamodel/Month.java
@@ -0,0 +1,5 @@
+package com.webservices.datamodel;
+
+public class Month {
+
+}

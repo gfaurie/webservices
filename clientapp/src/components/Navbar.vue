5d4f69aeeda08bf395783b26b709de1ccd73ec38
Etienne DUMONTET <etiennedumontet@hotmail.com>
2021-05-12 09:36:41 +0200

    Structure et modules front

---
+++ clientapp/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="navbar">
+    <b-navbar>
+      <b-navbar-brand href="#">Time Management</b-navbar-brand>
+      <!-- Align items to the left -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item v-if="isAuthenticated" v-on:click="onChangeAuth()">
+          Se déconnecter
+        </b-nav-item>
+        <b-nav-item v-if="!isAuthenticated" v-on:click="onChangeAuth()">
+          Se connecter
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    isAuthenticated: Boolean,
+  },
+  methods: {
+    onChangeAuth() {
+      this.isAuthenticated = !this.isAuthenticated;
+    },
+  },
+};
+</script>
+
+<style>
+.navbar {
+  background-color: #7799dd;
+}
+a.nav-link {
+  padding-left: 10px;
+  padding-right: 10px;
+  color: black !important;
+}
+a.nav-link:hover {
+  text-decoration: none;
+  border-radius: 5px;
+  background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1));
+}
+</style>
+
---
+++ clientapp/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <b-form class="form-reg" @submit="onSubmit">
+        <b-form-group
+          id="username-group"
+          label="Nom d'utilisateur : "
+          label-for="username-input"
+        >
+          <b-form-input
+            id="username-input"
+            v-model="loginForm.username"
+            type="text"
+            required
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="password-group"
+          label="Mot de passe : "
+          label-for="password-input"
+        >
+          <b-form-input
+            id="password-input"
+            v-model="loginForm.password"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" class="primary">Se connecter</b-button>
+        <b-button type="button" class="secondary">S'inscrire</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an   to /src
+// import UserService from '@/services/userService.js';
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      console.log(this.loginForm);
+    },
+  },
+};
+</script>
+
+<style>
+body, .container {
+  height: 100%;
+}
+.form-reg {
+  margin: auto;
+  max-width: 400px;
+}
+</style>

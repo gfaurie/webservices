5ef57e79f365db05cc0a249d8796be3e065b210d
Gabrielle Faurie <gabrielle.faurie@savoye.com>
2021-05-28 14:35:14 +0200

    Connect soap test

--- main-project/time/src/main/java/com/webservices/application/MySQLConnect.java
+++ main-project/time/src/main/java/com/webservices/application/MySQLConnect.java
@@ -34,38 +34,5 @@ public class MySQLConnect extends AbstractJdbcConfiguration {
         dataSource.setUsername("root");
         dataSource.setPassword("");
         return dataSource;
-    }
-    
-    
-	/*public static void main(String args[]){  
-		String sqlSelectAllPersons = "SELECT * FROM user";
-		String sqlInsertPerson = "INSERT INTO user (id_user, login, password, name, last_name, is_manager)"
-				+ "VALUES (1, 'gab', 'gab1', 'Gabrielle', 'Faurie', 1)";
-		String connectionUrl = "jdbc:mysql://localhost:3306/web_services?serverTimezone=UTC";
-		//int result = 0;
-		
-		try {
-
-			Connection conn = DriverManager.getConnection(connectionUrl, "root", ""); 
-	        PreparedStatement ps = conn.prepareStatement(sqlInsertPerson); 
-	        //ResultSet rs = ps.executeQuery();
-	        int result = ps.executeUpdate();
-		        while (rs.next()) {
-		            long id = rs.getLong("id_user");
-		            String name = rs.getString("name");
-		            String lastName = rs.getString("LAST_NAME");
-		            
-		            System.out.println(id + name + lastName);
-
-		            // do something with the extracted data...
-		        }
-			
-			System.out.println(result);
-		        
-		} catch (SQLException e) {
-			System.out.println(e);
-		    // handle the exception
-		}
-	}*/
- 	
+    } 	
 }
